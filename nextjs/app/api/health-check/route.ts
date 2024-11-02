9e252b81c4579ebb59df23ae30b684da88cd3e07
hochituong7 <hochituong7@gmail.com>
2024-11-03 00:03:13 +0700

    health check

---
+++ nextjs/app/api/health-check/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest) {
+  try {
+    return NextResponse.json(`${request.url} OK`)
+  } catch (e) {
+    console.log(e)
+    return NextResponse.json(`${request.url} NG`, {status: 500})
+  }
+}
