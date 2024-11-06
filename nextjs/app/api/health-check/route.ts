6620668ae558c6ac2180af90c3074090178de67b
hochituong7 <hochituong7@gmail.com>
2024-11-06 21:43:55 +0700

    auto CICD

--- nextjs/app/api/health-check/route.ts
+++ nextjs/app/api/health-check/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
   try {
+    
     return NextResponse.json(`${request.url} OK`)
   } catch (e) {
     console.log(e)
